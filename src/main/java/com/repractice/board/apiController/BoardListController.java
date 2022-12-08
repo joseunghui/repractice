de8ec0b5080c78201cd68df17f143e86544f8ce7
zelda <ooo167@naver.com>
2022-12-08 16:51:12 +0900

    BoardList 관련 추가 작업

--- src/main/java/com/repractice/board/apiController/BoardListController.java
+++ src/main/java/com/repractice/board/apiController/BoardListController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 import static com.repractice.board.controller.constants.BoardWebUrl.BOARDLIST;
 
