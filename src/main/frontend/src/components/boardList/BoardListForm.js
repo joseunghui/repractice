79d40e234605c9e71011924a3ca5226a91138ad2
zelda <ooo167@naver.com>
2022-12-09 16:14:44 +0900

    BoardList 관련 추가 작업

--- src/main/frontend/src/components/boardList/BoardListForm.js
+++ src/main/frontend/src/components/boardList/BoardListForm.js
@@ -1,27 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
 
 const BoardListForm = () => {
 
+    const [list, setList] = useState([]);
+
     axios.post('/board', null, null)
-        .then(() => {
+        .then( response => {
+            setList(response.data);
+            console.log(response.data);
         })
         .catch((Error) => {
             console.log("BoardListForm.js 오류")
         });
 
-    return (
-        <>
-            <h4>BoardListForm</h4>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
-        </>
-    )
+    const BoardList = ({list}) => {
+        return (
+            <div>
+                <h3>boardList</h3>
+            </div>
+        );
+    };
+
+
 };
 
 export default BoardListForm;
