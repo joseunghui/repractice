bfa82bd9f9614a2862e760139eae0b27d5a52c6f
zelda <ooo167@naver.com>
2022-12-12 10:17:39 +0900

    BoardList 관련 추가 작업

--- src/main/frontend/src/components/boardList/BoardListForm.js
+++ src/main/frontend/src/components/boardList/BoardListForm.js
@@ -17,18 +17,19 @@ const BoardListForm = () => {
 
 
     return (
-        <table style={{border : "1px solid", borderCollapse : "collapse"}}>
-            {list.map( board =>
-                    <tr style={{border : "1px solid"}}>
-                        <td>{board.title}</td>
-                        <td>{board.writer}</td>
-                        <td>{board.dateTime.slice(0, 10)}</td>
-                        <td>{board.content}</td>
-                    </tr>
-            )}
-        </table>
+        <>
+            <table style={{border : "1px solid", borderCollapse : "collapse"}}>
+                {list.map( board =>
+                        <tr style={{border : "1px solid"}}>
+                            <td>{board.title}</td>
+                            <td>{board.writer}</td>
+                            <td>{board.dateTime.slice(0, 10)}</td>
+                            <td>{board.content}</td>
+                        </tr>
+                )}
+            </table>
+        </>
     )
 };
-
 
 export default BoardListForm;
